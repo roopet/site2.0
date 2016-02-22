c2501bf5a7bc62f289e8ebd185a4224def4b155f
Atte <atte.pulkkinen@gmail.com>
2016-02-22 14:20:41 +0200

    added compiled files

--- bare/scripts/main.js
+++ bare/scripts/main.js
@@ -1,11 +1,11 @@
-(function($) {
+(function ($) {
     'use strict';
     $('a').smoothScroll();
 
     var windowHeight = $(window).height();
 
-    $(document).scroll(function() {
-        if(window.pageYOffset > (windowHeight / 3) * 2) {
+    $(document).scroll(function () {
+        if (window.pageYOffset > (windowHeight / 3) * 2) {
             $('.top-link-block').show();
         } else {
             $('.top-link-block').hide();
