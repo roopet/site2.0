831b7375b08fbea76e42ddf763be212d1481987e
Atte <atte.pulkkinen@gmail.com>
2016-02-22 11:05:54 +0200

    i dunno, maybe this works

---
+++ README.md
@@ -0,0 +1,22 @@
+## Vividin 
+
+
+### Installing dev enviroment
+
+Be sure you have [NPM](https://www.npmjs.com/) installed
+
+```
+npm install
+```
+
+### Using Gulp
+
+Start testing server on default address [http://localhost:9000/](http://localhost:9000/) 
+```
+gulp serve
+```
+
+Compiles production ready version
+```
+gulp build
+```
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+(function($) {
+    $('a').smoothScroll();
+
+    var windowHeight = $(window).height();
+
+    $(document).scroll(function() {
+        if(window.pageYOffset > (windowHeight / 3) * 2) {
+            $('.top-link-block').show();
+        } else {
+            $('.top-link-block').hide();
+        }
+    });
+
+})(jQuery);
